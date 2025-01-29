721781db26ae19c426008aca0b3a0b3cfd35deb6
S-Tatsuya <75727668+S-Tatsuya@users.noreply.github.com>
2025-01-30 05:25:41 +0900

    Chapter1を実施 (#1)

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  inputs = {
+    sub_flake.url = "path:./sub";
+  };
+
+  outputs = { sub_flake, ...}: {
+    hello = "Hello, world!";
+    sum_1_2 = sub_flake.add_a_b 1 2;
+    now = builtins.currentTime;
+  };
+}
---
+++ main.nix
@@ -0,0 +1,7 @@
+let
+  add = import ./add.nix;
+in
+  add {
+    a = 1;
+    b = 2;
+}
