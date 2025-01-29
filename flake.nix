1ed103a44aaf3f17de8094cbebece1cfe1c8183a
S-tatsuya <s.tatsuya0123@gmail.com>
2025-01-30 04:47:09 +0900

    sub Flakeに依存する処理を記載

--- flake.nix
+++ flake.nix
@@ -1,5 +1,10 @@
 {
-  outputs = _inputs: {
+  inputs = {
+    sub_flake.url = "path:./sub";
+  };
+
+  outputs = { sub_flake, ...}: {
     hello = "Hello, world!";
+    sum_1_2 = sub_flake.add_a_b 1 2;
   };
 }
