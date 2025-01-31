4ac037db0ccf73405abd0522ec6b828b2f0c9a36
S-tatsuya <s.tatsuya0123@gmail.com>
2025-02-01 06:40:06 +0900

    すべてのシステム向けにパッケージをビルド

--- nixpkags_test/flake.nix
+++ nixpkags_test/flake.nix
@@ -1,16 +1,19 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {nixpkgs, ...}:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      packages.${system} = {
-        hello = pkgs.hello;
-      };
-    };
+  outputs = {nixpkgs, flake-utils, ...}:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          hello = pkgs.hello;
+        };
+      }
+    );
 }
